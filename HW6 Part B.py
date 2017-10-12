7ffca3a21acef71e68edd79e9fd455d93369f009
Joseph Stempel <jstempel@umich.edu>
2017-10-12 18:12:42 -0400

    Final Commit

--- HW6 Part B.py
+++ HW6 Part B.py
@@ -9,9 +9,25 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
 
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+my_count = int(input('Enter count - '))
+my_position = (int(input('Enter position '))-1)
+
+
+def find_my_position(url, my_position):
+	print ('Retrieving: ' + str(url))
+	html = urllib.request.urlopen(url, context=ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	my_tags = soup.find_all('a')
+	return my_tags[my_position].get('href')
+
+for item in range(my_count):
+	url = find_my_position(url, my_position)
+
+final_html = urllib.request.urlopen(url, context=ctx).read()
+final_soup = BeautifulSoup(final_html, 'html.parser')
+print (final_soup.title.string.split(' ')[2])
+
+
+
+
